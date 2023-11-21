fb3d0939731112440dcdffad62b5dfe406774a01
lytran14 <139949514+lytran14@users.noreply.github.com>
2023-11-21 14:35:16 +0700

    formBanRa

---
+++ src/Class_Model/SanPham_Model.java
@@ -0,0 +1,96 @@
+package Class_Model;
+
+public class SanPham_Model {
+
+    String MASP;
+    String TENSP;
+    String MOTA;
+    double DONGIANHAP;
+    String LOAI;
+    String MALOAISP;
+    double DONGIABAN;
+    String DONVITINH;
+    
+
+    public SanPham_Model() {
+    }
+
+    public SanPham_Model(String MASP, String TENSP, String MOTA, double DONGIANHAP, String LOAI, String MALOAISP, double DONGIABAN, String DONVITINH) {
+        this.MASP = MASP;
+        this.TENSP = TENSP;
+        this.MOTA = MOTA;
+        this.DONGIANHAP = DONGIANHAP;
+        this.LOAI = LOAI;
+        this.MALOAISP = MALOAISP;
+        this.DONGIABAN = DONGIABAN;
+        this.DONVITINH = DONVITINH;
+    }
+
+    public String getMASP() {
+        return MASP;
+    }
+
+    public void setMASP(String MASP) {
+        this.MASP = MASP;
+    }
+
+    public String getTENSP() {
+        return TENSP;
+    }
+
+    public void setTENSP(String TENSP) {
+        this.TENSP = TENSP;
+    }
+
+    public String getMOTA() {
+        return MOTA;
+    }
+
+    public void setMOTA(String MOTA) {
+        this.MOTA = MOTA;
+    }
+
+    public double getDONGIANHAP() {
+        return DONGIANHAP;
+    }
+
+    public void setDONGIANHAP(double DONGIANHAP) {
+        this.DONGIANHAP = DONGIANHAP;
+    }
+
+    public String getLOAI() {
+        return LOAI;
+    }
+
+    public void setLOAI(String LOAI) {
+        this.LOAI = LOAI;
+    }
+
+    public String getMALOAISP() {
+        return MALOAISP;
+    }
+
+    public void setMALOAISP(String MALOAISP) {
+        this.MALOAISP = MALOAISP;
+    }
+
+    public double getDONGIABAN() {
+        return DONGIABAN;
+    }
+
+    public void setDONGIABAN(double DONGIABAN) {
+        this.DONGIABAN = DONGIABAN;
+    }
+
+    public String getDONVITINH() {
+        return DONVITINH;
+    }
+
+    public void setDONVITINH(String DONVITINH) {
+        this.DONVITINH = DONVITINH;
+    }
+
+   
+    
+    
+}
